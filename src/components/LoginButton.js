6bab820a5d4afbe0827426b6621e41c3cf4db1d4
harminder <harminder@iwebcode.design>
2022-04-12 14:11:01 +0530

    sdfsdf

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -4,7 +4,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect({ redirectUri: window.location.origin  })}>Log In</button>;
+  return <button onClick={() => loginWithRedirect({ redirectUri: 'https://harminder20222.github.io/auth0-org-test'  })}>Log In</button>;
 };
 
 export default LoginButton;
